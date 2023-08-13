66995bcd40a7669d00162c56347e96c240058dde
Jakub <jakub.glad@hotmail.com>
2023-08-13 16:38:11 +0200

    Day 22: Finished Pong
    Finished Day 22 by creating classic Ponge game.

--- Day 22 - Pong/paddle.py
+++ Day 22 - Pong/paddle.py
@@ -17,3 +17,22 @@ class Paddle(Turtle):
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
+
+# reduced part of code from main:
+
+# paddle = Turtle()
+# paddle.shape("square")
+# paddle.color("white")
+# paddle.shapesize(stretch_wid=5, stretch_len=1)
+# paddle.penup()
+# paddle.goto(350, 0)
+#
+#
+# def go_up():
+#     new_y = paddle.ycor() + 20
+#     paddle.goto(paddle.xcor(), new_y)
+#
+#
+# def go_down():
+#     new_y = paddle.ycor() - 20
+#     paddle.goto(paddle.xcor(), new_y)
