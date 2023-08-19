ed0783196ae05084250d0c5102a9c90d9e9e168d
Jakub <kuba.gladysz@hotmail.com>
2023-08-19 21:30:03 +0200

    Working with Pandas part 2
    Learning about pandas functionality, getting data, making new csv data files.

--- Day 25 - States Game/challenge/main.py
+++ Day 25 - States Game/challenge/main.py
@@ -16,4 +16,41 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# # print(data["temp"])
+# # print(type(data))
+# # print(type(data["temp"]))
+#
+#
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(len(temp_list))
+#
+# # average = sum(temp_list) / len(temp_list)
+# # print(average)
+#
+# # Get Data in Columns
+# print(data["temp"].mean())
+# print(data.condition)
+
+# Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+# print(monday.condition)
+# monday_temp = int(monday.temp)
+# monday_temp_F = monday_temp * 9/5 + 32
+# print(monday_temp_F)
+
+# Create a dataframe from scratch
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("new_data.csv")
