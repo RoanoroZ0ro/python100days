3109ed0927df68ca0ea5baf5966f7db1eb469f22
Lucas Z <LucasBear001@gmail.com>
2023-11-26 15:42:18 +0800

    3 digit multiplication

--- Day 2 - Tip Calculator/Exercise 1 - Data Types.py
+++ Day 2 - Tip Calculator/Exercise 1 - Data Types.py
@@ -1,13 +1,11 @@
 # 🚨 Don't change the code below 👇
-two_digit_number = input("Type a two digit number: ")
+three_digit_number = input("Type a three digit number: ")
 # 🚨 Don't change the code above 👆
 
 ####################################
 #Write your code below this line 👇
-
-first_digit = two_digit_number[0]
-second_digit = two_digit_number[1]
-result = int(first_digit) + int(second_digit)
-
-print(str(first_digit) + " + " + str(second_digit) + " = " + str(result))
+first_digit = three_digit_number[0]
+second_digit = three_digit_number[1]
+third_digit =  three_digit_number[2]
+result = int(first_digit) * int(second_digit) * int(third_digit)
 print(result)
